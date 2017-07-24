e75c0672b7c286553791d0971d358cb399a12f50
yequancai <775496282@qq.com>
2017-07-24 14:06:37 +0800

    1.连接数据库 2.json测试

--- src/main/java/com/tianjian/lrms/hello/HelloController.java
+++ src/main/java/com/tianjian/lrms/hello/HelloController.java
@@ -1,13 +1,22 @@
 package com.tianjian.lrms.hello;
 
 import com.jfinal.core.Controller;
+import com.jfinal.plugin.activerecord.Record;
+
+import java.util.List;
 
 /**
  * Created by yequancai on 2017/7/19.
  */
 public class HelloController extends Controller{
+    HelloService helloService = new HelloService();
     public void index(){
-        System.out.print("see urlPara:"+getPara("v0"));
-        renderText("Hello JFinal World....GitCommitPushTest");
+//        renderText("Hello JFinal World....GitCommitPushTest111...");
+        render("/index2.html");
+    }
+
+    public void staticCashFlow() {
+        List<Record> users = helloService.getUser();
+        renderJson(users);
     }
 }
--- src/main/java/com/tianjian/lrms/mainconfig/MainConfig.java
+++ src/main/java/com/tianjian/lrms/mainconfig/MainConfig.java
@@ -1,8 +1,11 @@
 package com.tianjian.lrms.mainconfig;
 
 import com.jfinal.config.*;
+import com.jfinal.plugin.activerecord.ActiveRecordPlugin;
+import com.jfinal.plugin.activerecord.CaseInsensitiveContainerFactory;
+import com.jfinal.plugin.activerecord.dialect.OracleDialect;
+import com.jfinal.plugin.c3p0.C3p0Plugin;
 import com.jfinal.template.Engine;
-
 import com.tianjian.lrms.hello.HelloController;
 
 /**
@@ -10,16 +13,15 @@ import com.tianjian.lrms.hello.HelloController;
  */
 public class MainConfig extends JFinalConfig{
 
-    //常量值
+    //配置常量值
     public void configConstant(Constants constants) {
-
         constants.setDevMode(true);//开启开发模式
     }
 
     //访问路由
     public void configRoute(Routes routes) {
-        routes.setBaseViewPath("appWeb");//设置视图渲染的基础路径
-        routes.add("hello",HelloController.class);//路由测试
+//        routes.setBaseViewPath("/lrmsWeb");//设置视图渲染的基础路径
+        routes.add("/hello",HelloController.class);//路由测试
     }
 
     @Override
@@ -29,7 +31,14 @@ public class MainConfig extends JFinalConfig{
 
     @Override
     public void configPlugin(Plugins plugins) {
-
+        C3p0Plugin c3p0Plugin = new C3p0Plugin("jdbc:oracle:thin:@172.168.171.250:1521:appdev",
+                "tjrpm","tjrpm");//c3p0数据源插件
+        c3p0Plugin.setDriverClass("oracle.jdbc.driver.OracleDriver");//配置Oracle驱动
+        plugins.add(c3p0Plugin);
+        ActiveRecordPlugin activeRecordPlugin = new ActiveRecordPlugin(c3p0Plugin);//ActiveRecord支持插件
+        plugins.add(activeRecordPlugin);
+        activeRecordPlugin.setDialect(new OracleDialect());//配置Oracle方言
+        activeRecordPlugin.setContainerFactory(new CaseInsensitiveContainerFactory());//配置字段名大小写不敏感容器工厂
     }
 
     @Override
